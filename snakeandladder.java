e4e83936da06b447676d7fceec7156c699bf0b14
Priyanka1099 <58952427+Priyanka1099@users.noreply.github.com>
2021-07-20 13:45:41 +0530

    Add files via upload

--- snakeandladder.java
+++ snakeandladder.java
@@ -1,67 +1,98 @@
-import java.util.Map;
-import java.util.Random;
+
 
 public class snakeandladder{
-	 private static final Map<Integer, Integer> snl = Map.ofEntries(
-		        Map.entry(4, 14),
-		        Map.entry(9, 31),
-		        Map.entry(17, 7),
-		        Map.entry(20, 38),
-		        Map.entry(28, 84),
-		        Map.entry(40, 59),
-		        Map.entry(51, 67),
-		        Map.entry(54, 34),
-		        Map.entry(62, 19),
-		        Map.entry(63, 81),
-		        Map.entry(64, 60),
-		        Map.entry(71, 91),
-		        Map.entry(87, 24),
-		        Map.entry(93, 73),
-		        Map.entry(95, 75),
-		        Map.entry(99, 78)
-		    );
-		    private static final boolean sixesThrowAgain = true;
-		    private static Random rand = new Random();
-		 
-		    private static int turn(int player, int square) {
-		        int square2 = square;
-		        while (true) {
-		            int roll = rand.nextInt(6) + 1;
-		            System.out.printf("Player %d, on square %d, rolls a %d", player, square2, roll);
-		            if (square2 + roll > 100) {
-		                System.out.println(" but cannot move.");
-		            } else {
-		                square2 += roll;
-		                System.out.printf(" and moves to square %d\n", square2);
-		                if (square2 == 100) return 100;
-		                Integer next = snl.getOrDefault(square2, square2);
-		                if (square2 < next) {
-		                    System.out.printf("Yay! Landed on a ladder. Climb up to %d.\n", next);
-		                    if (next == 100) return 100;
-		                    square2 = next;
-		                } else if (square2 > next) {
-		                    System.out.printf("Oops! Landed on a snake. Slither down to %d.\n", next);
-		                    square2 = next;
-		                }
-		            }
-		            if (roll < 6 || !sixesThrowAgain) return square2;
-		            System.out.println("Rolled a 6 so roll again.");
-		        }
-		    }
-		 
-		    public static void main(String[] args) {
-		        // three players starting on square one
-		        int[] players = {1, 1};
-		        while (true) {
-		            for (int i = 0; i < players.length; ++i) {
-		                int ns = turn(i + 1, players[i]);
-		                if (ns == 100) {
-		                    System.out.printf("Player %d wins!\n", i + 1);
-		                    return;
-		                }
-		                players[i] = ns;
-		                System.out.println();
-		            }
-		        }
-		    }
-		}    
+	public static void main(String[] args) {
+	int Player1_Position = 0;
+	int Player2_Position = 0;
+	int Ladder = 1;
+	int Snake = 2;
+	int No_Play = 3;
+	int Winning_Position = 100;
+	int count1 = 0;
+	int count2 = 0;
+	while (Player1_Position != Winning_Position && Player2_Position != Winning_Position) {
+
+//Rolling the dice to find player1 position and  player2 position	
+
+		int Dice1 = (int) (Math.random() * 6 + 1);
+		System.out.println("The first Player's Position after rolling the die is : " + Dice1);
+
+		int Dice2 = (int) (Math.random() * 6 + 1);
+		System.out.println("The second Player's Position after rolling the die is : " + Dice2);
+
+//Generating random numbers to get options
+
+		int playercheck1 = (int) Math.floor(Math.random() * 3 + 1);
+		int playercheck2 = (int) Math.floor(Math.random() * 3 + 1);
+
+//Get the count of the dice roll
+		count1++;
+		count2++;
+		System.out.println("The dice number of player1 is: " + Dice1);
+		System.out.println("The dice number of player2 is: " + Dice2);
+
+		switch (playercheck1) {
+		case 1:
+			Player1_Position = Player1_Position + Dice1;
+			System.out.println("The player position is :" + Player1_Position);
+			if (Player1_Position > Winning_Position) {
+
+				Player1_Position = Player1_Position - Dice1;
+			}
+			break;
+		case 2:
+			Player1_Position = Player1_Position - Dice1;
+			System.out.println("The player position is :" + Player1_Position);
+			break;
+		case 3:
+			Player1_Position = 0;
+			System.out.println("There is no play and the player position is :" + Player1_Position);
+
+//If the player goes below zero then the player has to restart from the original position
+
+			if (Player1_Position < 0) {
+				Player1_Position = 0;
+			}
+			System.out.println("The player position is: " + Player1_Position);
+			break;
+		}
+
+//Switch case for player2
+		switch (playercheck2) {
+		case 1:
+			Player2_Position = Player2_Position + Dice2;
+			System.out.println("The player position is :" + Player2_Position);
+			if (Player2_Position > Winning_Position) {
+
+				Player2_Position = Player2_Position - Dice2;
+			}
+			break;
+		case 2:
+			Player2_Position = Player2_Position - Dice2;
+			System.out.println("The player position is :" + Player2_Position);
+			break;
+		case 3:
+			Player2_Position = 0;
+			System.out.println("There is no play and the player position is :" + Player2_Position);
+
+//If the player goes below zero then the player has to restart from the original position
+
+			if (Player2_Position < 0) {
+				Player2_Position = 0;
+			}
+			System.out.println("The player position is: " + Player2_Position);
+			break;
+		}
+
+	}
+	if (Player1_Position == Winning_Position) {
+		System.out.println("Player1 is winner");
+		System.out.println("Number of times die rolled by player1: " + count1);
+	} else {
+		System.out.println("Player2 is winner");
+		System.out.println("Number of times die rolled by player2: " + count2);
+	}
+
+}
+
+}
